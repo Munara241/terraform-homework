923b95585d4f265bc32210f9497004a0daf74125
Munara241 <munara.24185@gmail.com>
2024-04-07 00:06:07 +0000

    create folder

---
+++ Homework3/main.tf
@@ -0,0 +1,13 @@
+# Description
+
+# Create Homework3 folder and perform following:
+# Provision 3 EC2 instances with one resource block in the us-west-2 region.
+# Instances type should be t2.micro.
+# Use the latest Amazon Linux 2 AMI for the instance.
+# Create security group with inbounds ports 22, 80, 443 and 3306 open and attach to the Instances.
+# Add Bastion key to the Instances.
+# Ensure EC2 instances are accessible over the internet. Assign a public IP address.
+# Ensure EC2 instances created in each AZ.
+# Ensure instances names are: web-1, web-2, web-2.
+# Automatically install Apache web server and create a simple webpage that returns "Hello, World!" when visited.
+# Provide GitHub URL in the comment below.
