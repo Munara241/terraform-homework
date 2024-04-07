de6acbb4d9e5d468eb36dae7d16f0c6392038a68
Munara241 <munara.24185@gmail.com>
2024-04-07 21:42:28 +0000

    edit

--- Homework3/httpd.sh
+++ Homework3/httpd.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum install apache2 -y
+sudo yum install httpd -y
 sudo systemctl start httpd
 sudo systemctl enable httpd
-echo “Hello World” > /var/www/html/index.html
+sudo echo “Hello World” > /var/www/html/index.html
 
