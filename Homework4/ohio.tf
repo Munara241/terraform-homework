3d13ebd5d55580d19efdd36127d3dfe90a175e72
munara <munara.24185@gmail.com>
2024-04-11 01:36:08 +0000

    added HW4

---
+++ Homework4/ohio.tf
@@ -0,0 +1,10 @@
+region             = "us-east-2"
+ami_id             = "ami-0900fe555666598a2"
+type               = "t2.micro"
+key                = "aws_key_pair.pub_key"
+availability_zones = "us-east-2a"
+count_vm = "1"
+
+
+
+
