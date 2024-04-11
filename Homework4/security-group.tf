75f8453a4e751bbd3a8778c54a80f3f92367b095
munara <munara.24185@gmail.com>
2024-04-11 22:34:35 +0000

    ready

--- Homework4/security-group.tf
+++ Homework4/security-group.tf
@@ -1,53 +1,30 @@
-variable "ingress_rules" {
-  default = {
-    "80" = {
-      description = "For HTTP"
-      from_port   = 80
-      to_port     = 80
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    },
-    "22" = {
-      description = "For SSH"
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    },
-    "8080" = {
-      description = "For HTTPS"
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  }
-  type = map(object({
-    description = string
-    from_port   = number
-    to_port     = number
-    protocol    = string
-    cidr_blocks = list(string)
-  }))
-  description = "Allow TLS inbound traffic"
-}
 
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
- 
 
-  dynamic "ingress" {
-    for_each = var.ingress_rules
-    content {
-      description = lookup(ingress.value, "description", null)
-      from_port   = lookup(ingress.value, "from_port", null)
-      to_port     = lookup(ingress.value, "to_port", null)
-      protocol    = lookup(ingress.value, "protocol", null)
-      cidr_blocks = lookup(ingress.value, "cidr_blocks", null)
-    }
+ ingress {  # inbound rules incoming traffic
+    description      = "provide port 22"
+    from_port        = var.ports[0]
+    to_port          = var.ports[0]
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {  # inbound rules incoming traffic
+    description      = "provide port 80"
+    from_port        = var.ports[1]
+    to_port          = var.ports[1]
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
+  ingress {  # inbound rules incoming traffic
+    description      = "provide port 443"
+    from_port        = var.ports[2]
+    to_port          = var.ports[2]
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
